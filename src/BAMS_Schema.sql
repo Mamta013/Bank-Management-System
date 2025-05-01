5e5d5de0ceb55a12efac7fbb4bc67ef3b85a5c21
Mamta013 <102298223+Mamta013@users.noreply.github.com>
2025-05-01 13:35:08 +0530

    Update BAMS_Schema.sql

--- src/BAMS_Schema.sql
+++ src/BAMS_Schema.sql
@@ -1,7 +1,6 @@
 -- Active: 1686788664084@@127.0.0.1@5432@bank_accounts_management_systems
 /* Bank Accounts Management System
 Schema & Tables
-© 2023 Tushar Aggarwal. All rights reserved.
 */
 CREATE DATABASE bank_accounts_management_systems;
 
